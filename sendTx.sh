324050d0b3612c221305ccc4f22a976f131e7b2a
MoLin <molin@wanchain.org>
2019-07-15 18:31:09 +0800

    u

--- sendTx.sh
+++ sendTx.sh
@@ -7,10 +7,10 @@ ipArray=($(awk '{print $1}' ip.txt))
 index=16
 for i in {1..4}
 do 
-  echo ${index} ${ipArray[$index]} ${addrArray[$index]} 
+  echo ${index} ${ipArray[$index]} ${addrArray[$index]} ${pKeyArray[$index]}
   
-  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ipArray[$index]} "git clone https://github.com/wanchain/scanPos"
-  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ipArray[$index]} "cd scanPos && git checkout tps && git pull && npm install"
+  #ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ipArray[$index]} "git clone https://github.com/wanchain/scanPos"
+  #ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ipArray[$index]} "cd scanPos && git checkout tps && git pull && npm install"
   ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ipArray[$index]} "nohup ~/tx.sh ${addrArray[$index]} ${pKeyArray[$index]} >~/tx.log 2>&1 &"
   ((index++))
 done
