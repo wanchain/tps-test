5ea99e51e15aa10dc5713db18c9b309a74274d34
MoLin <molin@wanchain.org>
2019-07-15 15:27:03 +0800

    update

--- install.sh
+++ install.sh
@@ -7,3 +7,12 @@ do
   ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash - && sudo apt-get install -y nodejs gcc g++ make && sudo npm install -g -y pm2"
   ((index++))
 done
+
+index=0
+for ip in $(cat ip.txt)
+do 
+  echo ${index} ${ip}
+  scp -o StrictHostKeyChecking=no -i ${pem} ./keys.tar.gz ubuntu@${ip}:~/
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "mkdir -p data/keystore && tar -C ~/data/keystore -xzf ~/keys.tar.gz"
+  ((index++))
+done
--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 curl ifconfig.me
+echo ""
 echo $1
+
+echo "wanglu" > ~/pw.txt
+nohup ~/gwan --internal --mine --etherbase $1 --unlock $1 --password ~/pw.txt --datadir ~/data
