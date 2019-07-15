1395233eafaeeafbbeba3dbeaf244f7569a2b6cf
MoLin <molin@wanchain.org>
2019-07-15 15:59:02 +0800

    update

--- start.sh
+++ start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 pem=$(cat pem.txt)
 
+addrArray=($(awk '{print $1}' address.txt))  
+
+index=0
 for ip in $(cat ip.txt)
 do 
   echo ${index} ${ip}
-  ((index++))
+  addr=$($addrArray[${index}])
+  echo ${addr}
   ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "nohup ./run.sh ${addr} >~/out.log 2>&1 &"
+  ((index++))
 done
