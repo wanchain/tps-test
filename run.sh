9c9adda70d78f83249d9dc8112ddce2786718661
Mo Lin <molin@wanchain.org>
2019-07-16 11:50:57 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $1
 echo "wanglu">~/pw.txt
-nohup ~/gwan --internal --mine --etherbase $1 --unlock $1 --password ~/pw.txt --datadir ~/data
+nohup ~/gwan --internal --mine --etherbase $1 --unlock $1 --password ~/pw.txt --datadir ~/data --verbosity 5
 echo "start finish"
 curl ifconfig.me 
 echo ''
