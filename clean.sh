e9fca5fc3be3c85e4a18fb870143df9dbb65b1e6
Mo Lin <molin@wanchain.org>
2019-07-17 11:24:38 +0800

    update clean.sh

--- clean.sh
+++ clean.sh
@@ -7,6 +7,6 @@ do
   echo ${index} ${ip}
   ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "rm -rf ~/data/gwan && mkdir -p ~/data/gwan"
   scp -o StrictHostKeyChecking=no -i ${pem} ./static-nodes.json ubuntu@${ip}:~/data/gwan/
-  scp -o StrictHostKeyChecking=no -i ${pem} ./nodekeys/n${index} ubuntu@${ip}:~/data/gwan/nodekey
+  scp -o StrictHostKeyChecking=no -i ${pem} ./nodekeys/n${index+1} ubuntu@${ip}:~/data/gwan/nodekey
   ((index++))
 done
