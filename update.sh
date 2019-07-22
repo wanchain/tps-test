4f0094fde5ff200bed992c668a291439d5cbbb72
Mo Lin <molin@wanchain.org>
2019-07-22 12:14:57 +0800

    update branch to posperf

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pem=$(cat pem.txt)
 
-cd ~/go-wanchain && git checkout develop && git pull && make release
+cd ~/go-wanchain && git checkout posperf && git pull && make release
 
 cd ~/tpsTest
 
