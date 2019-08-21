3c93577cb367ab34493db311b132063fa039bbd1
Mo Lin <molin@wanchain.org>
2019-08-21 20:07:06 +0800

    update sh

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 pem=$(cat pem.txt)
 
-cd ~/go-wanchain && git pull &&git checkout pf2 && git pull && make release
+cd ~/go-wanchain && git pull &&git checkout tps-test && git pull && make release
 
 cd ~/tpsTest
 
