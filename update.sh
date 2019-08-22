54741d7eb095caf732f1d8f035eba3cce29065ec
Mo Lin <molin@wanchain.org>
2019-08-22 12:12:32 +0800

    update

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 pem=$(cat pem.txt)
 
-cd ~/go-wanchain && git pull &&git checkout tps-test && git pull && make release
+cd ~/go-wanchain && git pull &&git checkout tps-test && git pull && make
 
-cd ~/tpsTest
+cd ~/tps-test
 
 for ip in $(cat ip.txt)
 do 
   echo ${index} ${ip}
   ((index++))
-  scp -o StrictHostKeyChecking=no -i ${pem} ~/go-wanchain/build/bin/gwan-linux-amd64 ubuntu@${ip}:~/gwan
-  scp -o StrictHostKeyChecking=no -i ${pem} ~/tpsTest/run.sh ubuntu@${ip}:~/ 
-  scp -o StrictHostKeyChecking=no -i ${pem} ~/tpsTest/tx.sh ubuntu@${ip}:~/ 
-  #scp -o StrictHostKeyChecking=no -i ${pem} ~/tpsTest/watch.sh ubuntu@${ip}:~/ 
+  scp -o StrictHostKeyChecking=no -i ${pem} ~/go-wanchain/build/bin/gwan ubuntu@${ip}:~/gwan
+  scp -o StrictHostKeyChecking=no -i ${pem} ~/tps-test/run.sh ubuntu@${ip}:~/ 
+  scp -o StrictHostKeyChecking=no -i ${pem} ~/tps-test/tx.sh ubuntu@${ip}:~/ 
+  #scp -o StrictHostKeyChecking=no -i ${pem} ~/tps-test/watch.sh ubuntu@${ip}:~/ 
 
 done
 
