c5e8954cbab299c686700ff9e668ec815326ff8e
Mo Lin <molin@wanchain.org>
2019-10-23 14:30:50 +0800

    Update ip and netlimit.

--- limitSpeed.sh
+++ limitSpeed.sh
@@ -4,6 +4,9 @@ pem=$(cat pem.txt)
 for ip in $(cat ip.txt)
 do 
   echo ${index} ${ip}
-  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo wondershaper ens5 20480 20480"
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables --flush"
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A INPUT -m hashlimit --hashlimit-above 10240kb/s --hashlimit-mode srcip --hashlimit-name in -j DROP"
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A OUTPUT -m hashlimit --hashlimit-above 10240kb/s --hashlimit-mode dstip --hashlimit-name out -j DROP"
+
   ((index++))
 done
