1498f1510b0d7eba918761aa0afeff1d35ce4a87
Mo Lin <molin@wanchain.org>
2019-10-30 20:51:49 +0800

    speed 10MB/s

--- limitSpeed.sh
+++ limitSpeed.sh
@@ -5,8 +5,8 @@ for ip in $(cat ip.txt)
 do 
   echo ${index} ${ip}
   ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables --flush"
-  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A INPUT -m hashlimit --hashlimit-above 20480kb/s --hashlimit-mode srcip --hashlimit-name in -j DROP"
-  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A OUTPUT -m hashlimit --hashlimit-above 20480kb/s --hashlimit-mode dstip --hashlimit-name out -j DROP"
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A INPUT -m hashlimit --hashlimit-above 10240kb/s --hashlimit-mode srcip --hashlimit-name in -j DROP"
+  ssh -o StrictHostKeyChecking=no -i ${pem} ubuntu@${ip} "sudo iptables -A OUTPUT -m hashlimit --hashlimit-above 10240kb/s --hashlimit-mode dstip --hashlimit-name out -j DROP"
 
   ((index++))
 done
